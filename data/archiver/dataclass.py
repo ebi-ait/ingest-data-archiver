149370260b2c4a07dcdf3d17b6e59ceea81495e1
prabh-t <prabhat@ebi.ac.uk>
2022-01-11 13:08:28 +0000

    data archive result object & functions

--- data/archiver/dataclass.py
+++ data/archiver/dataclass.py
@@ -1,5 +1,6 @@
 from typing import List, Dict
 from dataclasses import dataclass
+import json
 
 
 class DataArchiverRequestParseExpection(Exception):
@@ -25,10 +26,35 @@ class FileResult:
     file_name: str
     md5: str
     success: bool
+    error: str
 
 
 @dataclass
 class DataArchiverResult:
     sub_uuid: str
     success: bool
+    error: str
     files: List[FileResult]
+
+    @staticmethod
+    def empty(req: DataArchiverRequest):
+        res_files = []
+        if req.files:
+            for file in req.files:
+                res_files.append(FileResult(file, None, True, None))
+        return DataArchiverResult(req.sub_uuid, True, None, res_files)
+
+    def update_file_result(self, file_name, md5, success, error):
+        for file in self.files:
+            if file.file_name == file_name:
+                file.md5 = md5
+                file.success = success
+                file.error = error
+                break
+
+    @staticmethod
+    def error_res(req, err):
+        return DataArchiverResult(req.sub_uuid, False, err, [])
+
+    def to_dict(self):
+        return json.loads(json.dumps(self, default=lambda o: o.__dict__))
