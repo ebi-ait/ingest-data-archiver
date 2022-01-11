c34bb649c36bfdbe889071beed6790ba1b76ca35
prabh-t <prabhat@ebi.ac.uk>
2022-01-11 13:07:06 +0000

    aws add only s3 files in initial request & update file result at end of download

--- data/archiver/aws_s3_client.py
+++ data/archiver/aws_s3_client.py
@@ -1,21 +1,29 @@
 import os
 import boto3
+import logging
+from data.archiver.dataclass import DataArchiverRequest, DataArchiverResult
 from data.archiver.file_transfer import FileTransfer, TransferProgress, transfer
 from data.archiver.config import AWS_S3_REGION, AWS_S3_BUCKET, AWS_ACCESS_KEY, AWS_SECRET_KEY
 
 
 class AwsS3:
 
-    def get_files(self, submission_uuid):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
 
+    def get_files(self, req: DataArchiverRequest, res: DataArchiverResult):
         bucket = self.new_session().resource('s3').Bucket(AWS_S3_BUCKET)
 
         fs = []
-        for obj in bucket.objects.filter(Prefix=submission_uuid):
+        for obj in bucket.objects.filter(Prefix=req.sub_uuid):
             # skip the top-level directory
-            if obj.key == submission_uuid:
+            if obj.key == req.sub_uuid:
                 continue
-            fs.append(FileTransfer(path=os.getcwd(), key=obj.key, size=obj.size))
+            f_name = obj.key.split('/')[1]
+            if f_name in req.files:
+                fs.append(FileTransfer(path=os.getcwd(), key=obj.key, size=obj.size))
+
 
         def download(idx):
             try:
@@ -34,8 +42,7 @@ class AwsS3:
                     fs[idx].successful = True
 
             except Exception as thread_ex:
-                print(thread_ex) 
-                print()
+                
                 if 'Forbidden' in str(thread_ex) or 'AccessDenied' in str(thread_ex):
                     fs[idx].status = 'Access denied.'
                 else:
@@ -43,15 +50,15 @@ class AwsS3:
                 fs[idx].complete = True
                 fs[idx].successful = False
 
-
-        print('Downloading...')
-
+        self.logger.info('Downloading...')
         transfer(download, fs)
 
         files = [f.key for f in fs if f.successful]
-        print(f'{len(files)}/{len(fs)} files downloaded.')
+        self.logger.info(f'{len(files)}/{len(fs)} files downloaded.')
 
-        return files
+        for f in fs:
+            f_name = f.key.split('/')[1]
+            res.update_file_result(f_name, None, f.successful, None if f.successful else f.status)
 
     def new_session(self):
         return boto3.Session(region_name=AWS_S3_REGION,
