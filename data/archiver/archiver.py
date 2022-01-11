5d7d259544efecd6a5eb81007c8649e6b26cbdf1
prabh-t <prabhat@ebi.ac.uk>
2022-01-11 13:06:18 +0000

    fixed localcopy archiving flow

--- data/archiver/archiver.py
+++ data/archiver/archiver.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from data.archiver.aws_s3_client import AwsS3
 from data.archiver.ftp_uploader import FtpUploader
 from data.archiver.utils import compress, md5
@@ -11,50 +12,83 @@ class Archiver:
         self.ingest_api = Ingest()
         self.aws_client = AwsS3()
         self.ftp = None
+        
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
 
     def start(self, req: DataArchiverRequest):
 
+        self.logger.info(f'Getting sequence files for submission {req.sub_uuid} from INGEST_API')
+        submission_files = self.ingest_api.get_sequence_files(req.sub_uuid)
+        self.logger.info(submission_files)
+
+        if not submission_files:
+            return DataArchiverResult.error_res('No sequence files for submission')
+
         if not req.files:
-            # get all sequence files for submission
-            req.files = self.ingest_api.get_sequence_files(req.sub_uuid)
+            # archive all files
+            req.files = submission_files
         
-        print(req)
-        # TODO logic here to decide between local copy or stream archive/upload to ena
-        self.archive_files_via_localcopy(req.sub_uuid)
+        self.logger.info(req)
+        res = DataArchiverResult.empty(req)
 
-    def archive_files_via_localcopy(self, uuid):
-        res = DataArchiverResult()
+        try:
+            # TODO logic here to decide between local copy or stream archive/upload to ena
+            result = self.archive_files_via_localcopy(req, res)
+            self.logger.info(result)
+            return result
 
-        # step 1 get a list of files to be uploaded
-        files = self.aws_client.get_files(uuid)
-        
-        # step 2 compress files using gz
-        print('Compressing...')
-        def maybe_compress(f):
-            if (f.endswith('.gz')):
-                print(f'Skipping {f}')
-                return f
+        except Exception as ex:
+            print(ex)
+
+    def archive_files_via_localcopy(self, req: DataArchiverRequest, res: DataArchiverResult):
+
+        self.logger.info(f'# step 1 download sequence files to be uploaded to FTP from S3')
+        self.aws_client.get_files(req, res)
+
+        self.logger.info(f'# step 2 compress files using gz')
+        def try_compress(f):
+            if (f.file_name.endswith('.gz')):
+                self.logger.info(f'Skipping {f.file_name}')
             else:
-                print(f'Compressing {f}')
-                compress(f)
-                return f'{f}.gz'
+                self.logger.info(f'Compressing {f.file_name}')
+                try:
+                    compress(f)
+                    f.file_name = f'{f.file_name}.gz'
+                except:
+                    f.success = False
+                    f.error = 'Compression failed'
 
-        compressed_files = list(map(maybe_compress, files))
+        for f in res.files:
+            if f.success:
+                try_compress(f)
+
         
-        # step 3 calculate checksums of compressed files (and create .md5 files)
-
+        self.logger.info(f'# step 3 calculate checksums of compressed files (and create .md5 files)')
+        checksum_files = []
         def calc_checksum(f):
-            print(f'Generating checksum {f}')
-            md5_file = f'{f}.md5'
+            self.logger.info(f'Generating checksum {f.file_name}')
+            md5_file = f'{req.sub_uuid}/{f.file_name}.md5'
             with open(md5_file,'a') as f1:
-                f1.write(md5(f))
-            return md5_file
+                md5_str = md5(f'{req.sub_uuid}/{f.file_name}')
+                f1.write(md5_str)
+                f.md5 = md5_str
+            checksum_files.append(md5_file) 
 
-        checksums = list(map(calc_checksum, compressed_files))
+        for f in res.files:
+            if f.success:
+                calc_checksum(f)
 
-        # step 4 upload files to Webin upload area
-        self.ftp = FtpUploader(uuid)
-        self.ftp.upload(compressed_files + checksums)
+        self.logger.info(f'# step 4 upload files to Webin upload area')
+        
+        self.ftp = FtpUploader(req, res)
+        self.ftp.upload()
+
+        for f in res.files:
+            res.success = res.success and f.success
+
+        return res
+
 
     def close(self):
         self.ingest_api.close()
