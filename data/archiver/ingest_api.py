d3ab48b3ea00dda9735231c052da24840a02798b
prabh-t <prabhat@ebi.ac.uk>
2022-01-18 01:03:22 +0000

    cache submission and files json, get staging area

--- data/archiver/ingest_api.py
+++ data/archiver/ingest_api.py
@@ -21,36 +21,54 @@ class Ingest:
 
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
+        self.submission = None
+        self.files = None
 
     @handle_exception
     def get_submission(self, uuid):
         submission_url = f'{INGEST_API}submissionEnvelopes/search/findByUuidUuid?uuid={uuid}'
         self.logger.info(f'Submission url {submission_url}')
         response = self.session.get(submission_url)
-        submission_json = json.loads(response.text)
-        return submission_json
+        self.submission = json.loads(response.text)
+        return self.submission
 
     @handle_exception
     def get_files(self, uuid):
-        submission = self.get_submission(uuid)
-        files_url = submission['_links']['files']['href']
+        if not self.submission:
+            self.get_submission(uuid)
+        files_url = self.submission['_links']['files']['href']
         self.logger.info(f'Files url {files_url}')
         response = self.session.get(files_url)
-        files_json = json.loads(response.text)
-        return files_json
+        self.files = json.loads(response.text)
+        return self.files
 
     @handle_exception
     def get_sequence_files(self, uuid):
-        files = self.get_files(uuid)
-        s3_files = []
+        if not self.files:
+            self.get_files(uuid)
+        self.s3_files = []
 
-        for file in files['_embedded']['files']:
+        for file in self.files['_embedded']['files']:
             if (file['content']['describedBy']).endswith('sequence_file'):
                 file_name = file['content']['file_core']['file_name']
-                s3_files.append(file_name)
+                cloud_url = file['cloudUrl']
+                self.s3_files.append({"file_name": file_name, "cloud_url": cloud_url})
     
-        return s3_files
+        return self.s3_files
+
+    def get_staging_area(self):
+        if self.submission and self.submission['stagingDetails']:
+            return self.submission['stagingDetails']['stagingAreaLocation']['value']
+        return None
 
     def close(self):
         self.session.close()
-    
+
+
+#if __name__ == "__main__": # python -m data.archiver.ingest_api
+def test_ingest_api():
+    ingest = Ingest()
+    ingest.get_submission('14df1f92-155c-4da2-97fc-85601dee64da')
+    if ingest.submission:
+        print(ingest.submission)
+        print(ingest.get_staging_area())
