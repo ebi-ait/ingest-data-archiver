542d76c297c7290240eca8c0613784d6aa020daa
prabh-t <prabhat@ebi.ac.uk>
2022-01-18 01:05:22 +0000

    clean up

--- data/archiver/archiver.py
+++ data/archiver/archiver.py
@@ -3,10 +3,9 @@ import logging
 from data.archiver.aws_s3_client import AwsS3
 from data.archiver.ftp_uploader import FtpUploader
 from data.archiver.utils import compress, md5
-from data.archiver.dataclass import DataArchiverRequest, DataArchiverResult
+from data.archiver.dataclass import DataArchiverRequest, DataArchiverResult, FileResult
 from data.archiver.ingest_api import Ingest
 
-STREAM_ARCHIVE = False
 
 class Archiver:
 
@@ -19,37 +18,42 @@ class Archiver:
         self.logger.setLevel(logging.INFO)
 
     def start(self, req: DataArchiverRequest):
-
+        self.logger.info(req)
         self.logger.info(f'Getting sequence files for submission {req.sub_uuid} from INGEST_API')
-        submission_files = self.ingest_api.get_sequence_files(req.sub_uuid)
-        self.logger.info(submission_files)
+        sequence_files = self.ingest_api.get_sequence_files(req.sub_uuid)
+        self.logger.info(sequence_files)
 
-        if not submission_files:
-            return DataArchiverResult.error_res('No sequence files for submission')
+        if not sequence_files:
+            res = DataArchiverResult(req.sub_uuid, success=False, error='No sequence files in submission')
+            self.logger.info(res)
+            return res
 
         if not req.files:
             # archive all files
-            req.files = submission_files
+            res_files = list(map(lambda f: FileResult(f["file_name"], f["cloud_url"]), sequence_files))
+        else:
+            staging_area = self.ingest_api.get_staging_area()
+            res_files = list(map(lambda f: FileResult(f, staging_area + f), req.files))
         
-        self.logger.info(req)
-        res = DataArchiverResult.empty(req)
+        res = DataArchiverResult(req.sub_uuid, files=res_files)
+        self.logger.info(res)
 
         try:
             # TODO logic here to decide between local copy or stream archive/upload to ena
-            if STREAM_ARCHIVE:
+            if req.stream:
                 return
             else:
-                result = self.archive_files_via_localcopy(req, res)
+                result = self.archive_files_via_localcopy(res)
                 self.logger.info(result)
                 return result
 
         except Exception as ex:
             print(ex)
 
-    def archive_files_via_localcopy(self, req: DataArchiverRequest, res: DataArchiverResult):
+    def archive_files_via_localcopy(self, res: DataArchiverResult):
 
-        self.logger.info(f'# step 1 download sequence files to be uploaded to FTP from S3')
-        self.aws_client.get_files(req, res)
+        self.logger.info(f'# step 1 download sequence files from S3')
+        self.aws_client.get_files(res)
 
         self.logger.info(f'# step 2 compress files using gz')
         def try_compress(f):
@@ -73,9 +77,9 @@ class Archiver:
         checksum_files = []
         def calc_checksum(f):
             self.logger.info(f'Generating checksum {f.file_name}')
-            md5_file = f'{req.sub_uuid}/{f.file_name}.md5'
+            md5_file = f'{res.sub_uuid}/{f.file_name}.md5'
             with open(md5_file,'a') as f1:
-                md5_str = md5(f'{req.sub_uuid}/{f.file_name}')
+                md5_str = md5(f'{res.sub_uuid}/{f.file_name}')
                 f1.write(md5_str)
                 f.md5 = md5_str
             checksum_files.append(md5_file) 
@@ -84,9 +88,9 @@ class Archiver:
             if f.success:
                 calc_checksum(f)
 
-        self.logger.info(f'# step 4 upload files to Webin upload area')
+        self.logger.info(f'# step 4 upload files to ENA upload area')
         
-        self.ftp = FtpUploader(req, res)
+        self.ftp = FtpUploader(res)
         self.ftp.upload()
 
         for f in res.files:
